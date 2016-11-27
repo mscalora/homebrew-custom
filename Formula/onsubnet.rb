9b69a11408e37b657e8f4a4e61a3c21a810ee530
Mike <mscalora@users.noreply.github.com>
2016-11-26 19:05:15 -0700

    Switch to branch based versioning

--- Formula/onsubnet.rb
+++ Formula/onsubnet.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Onsubnet < Formula
   homepage 'https://github.com/mscalora/onsubnet'
-  url 'https://github.com/mscalora/onsubnet.git', :tag => '1.2'
+  url 'https://github.com/mscalora/onsubnet.git', :branch => '1.2'
   version '1.2'
   
   def install
