a2cc2c3e4c676ad70c7ca1741daede4181948d43
Mike <mscalora@users.noreply.github.com>
2016-11-26 19:04:44 -0700

    Update onsubnet.rb

--- Formula/onsubnet.rb
+++ Formula/onsubnet.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Onsubnet < Formula
   homepage 'https://github.com/mscalora/onsubnet'
-  url 'https://github.com/mscalora/onsubnet.git', :tag => '1.1'
-  version '1.1'
+  url 'https://github.com/mscalora/onsubnet.git', :tag => '1.2'
+  version '1.2'
   
   def install
     bin.install 'onsubnet'
